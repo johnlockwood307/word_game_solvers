2448d8d42b78ea61e732be9cb29d2b42b7e96742
John Lockwood <johnlockwood307@gmail.com>
2025-01-23 19:08:55 -0500

    added api/index.py

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <html>
     <head>
-        <title>TITLE</title>
-        <?php
-            echo shell_exec("python word_hunt.py \"abcd efgh qwer tyui\"");
-        ?>
+        <title>TITLE</title> 
     </head>
 
     <body>
         <h1>Heading</h1>
         <p>Paragraph</p>
-        <p>second paragraph</p>
+        <p>
+            second paragraph
+        </p>
     </body>
 
 </html>
