3a7f2b0de0d4261453dea0e0ea95f4788edd2c84
John Lockwood <johnlockwood307@gmail.com>
2025-07-21 12:39:23 -0400

    skeleton for anagrams done

--- js/anagrams.js
+++ js/anagrams.js
@@ -1,10 +1,33 @@
-const johnBtn = document.getElementById('JohnButton')
-const johnTxt = document.getElementById('JohnText')
+const enterLettersTxt = document.getElementById('enterLetters')
+const validityLbl = document.getElementById('validityLbl')
+const anagramsOutput = document.getElementById('anagramsOutput')
+let letters = ""
 
-johnBtn.addEventListener('click', john)
+enterLettersTxt.addEventListener('input', findWords)
 
-john()
+findWords()
 
-function john() {
-    johnTxt.innerHTML = "John says " + Date()
+
+/**
+ * Checks the validity of the inputted letters.
+ * @returns whether the input letters are alphabetic and there are 6 or 7 of them.
+ */
+function lettersValid() {
+    letters = enterLettersTxt.value.toLowerCase()
+    regex = /^[a-z]+$/
+    len = letters.length
+    return regex.test(letters) && (len == 6 || len == 7)
+}
+
+/**
+ * Finds valid anagrams words given inputted letters. 
+ * Outputs them in order of non-increasing length.
+ */
+function findWords() {
+    if(lettersValid()) {
+        validityLbl.innerHTML = "Valid."
+        anagramsOutput.innerHTML = "Words will appear here<br>and here<br>and here"
+    } else {
+        validityLbl.innerHTML = "Enter 6 or 7 valid letters"
+    }
 }
