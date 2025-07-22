5f3609cd5811fcaf46795f956256a757ed6d6907
John Lockwood <johnlockwood307@gmail.com>
2025-07-22 16:59:00 -0400

    build trie js function

--- js/trie.js
+++ js/trie.js
@@ -1,13 +1,32 @@
 let trie;
+import fs from 'fs';
+import readline from 'readline';
+
 
 class TrieNode {
     constructor(letter) {
         this.letter = letter;
         this.parent = NaN;
-        this.children = {}
+        this.children = {};
     }
 }
 
-function buildTrie() {
-    trie = TrieNode('<')
+async function buildTrie() {
+    trie = TrieNode('<');
+
+    const fileStream = fs.createReadStream('../collins_scrabble_words.txt');
+    const rl = readline.createInterface({input: fileStream});
+
+    for await (const upperWord of rl) {
+        lowerWord = upperWord.toLowerCase() + '>';
+        let curNode = trie;
+        for (let ltr of lowerWord) {
+            if(!(ltr in curNode.children)) {
+                curNode.children[ltr] = TrieNode(ltr);
+                curNode.children[ltr].parent = curNode;
+            }
+
+            curNode = curNode.children[ltr];
+        }
+    }
 }
